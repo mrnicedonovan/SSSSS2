93dd7669c99665276822b907cdea6631ab9109b7
Donovan Nguyen <donoboy97@hotmail.com>
2015-04-03 00:58:45 +0200

    Test
    test

---
+++ Assets/_Scripts/Player/Player2Features.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Features : MonoBehaviour
+{
+    //Animator animator;
+    private Renderer invis;
+    private bool invisible = false;
+    public float timeOfInvisi;
+    // Use this for initialization
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        invis = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animator.SetBool("attack", true);
+        bool shoot = Input.GetKeyDown(KeyCode.N);
+        shoot |= Input.GetKeyDown(KeyCode.M);
+
+        if (timeOfInvisi >= 5)
+        {
+            invis.enabled = true;
+
+            invisible = false;
+        }
+        if (shoot)
+        {
+            WeaponBehaviour weapon = GetComponent<WeaponBehaviour>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                if (invisible == false)
+                {
+                    timeOfInvisi++;
+                    invis.enabled = false;
+
+                    invisible = true;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                if (invisible == true)
+                {
+                    invis.enabled = true;
+
+                    invisible = false;
+                }
+            }
+
+        }
+   
+
+}
---
+++ Assets/_Scripts/Player/Player2Movement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Movement : MonoBehaviour
+{
+    Animator animator;
+    public float speed;
+    public float jumpSpeed;
+    public bool facingRight;
+    private bool isJumping;
+    private Rigidbody2D _Body;
+ 
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        _Body = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("trans1", true);
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (facingRight)
+            {
+                Flippie();
+            }
+            if (Input.GetKey(KeyCode.RightShift))
+            {
+                transform.Translate(Vector2.right * speed * 2 * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.right * -speed * Time.deltaTime);
+            animator.SetBool("trans1", true);
+            if (!facingRight)
+            {
+                Flippie();
+            }
+            if (Input.GetKey(KeyCode.RightShift))
+            {
+                transform.Translate(Vector2.right * -speed * 2 * Time.deltaTime);
+            }
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (isJumping == true)
+            {
+                //animator.SetTrigger("jump?");
+                _Body.velocity = Vector2.up * jumpSpeed;
+                
+                isJumping = false;
+            }
+
+
+        }
+
+
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D Collider2D)
+    {
+        if (Collider2D.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+            //animator.SetTrigger("Idle");
+        }
+    }
+
+    void Flippie()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
